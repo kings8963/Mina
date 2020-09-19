74cc93dca75104b2a4a80887589d3f024aec087b
Reyes Angel <kings7nnt@gmail.com>
2020-09-19 11:41:31 -0500

    primer commit

---
+++ Assets/scripts/DragAndDrop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerDownHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Pointer down");
+    }
+
+    void Start()
+    { 
+    
+    }
+
+    void Update()
+    { 
+    
+    }
+}
---
+++ Assets/scripts/carritoController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carritoController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] float speed = 1;
+    [SerializeField] float move = 1;
+    //  private bool canJump = false;
+    //private bool isGrounded = true;
+   // [SerializeField] float jumpForce = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCar();
+      
+
+    }
+
+    public void moveCar()
+    {
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        
+    }
+
+
+   
+}
